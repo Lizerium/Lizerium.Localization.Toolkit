826359e08d9e0d4b8a5737897b6003014c4c1270
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-15 07:50:30 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-15

--- src/Lizerium.Localization.Ai.Analyzer/StringLiteralAnalyzer.cs
+++ src/Lizerium.Localization.Ai.Analyzer/StringLiteralAnalyzer.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 14 мая 2026 10:37:33
- * Version: 1.0.28
+ * Last Updated: 15 мая 2026 07:50:20
+ * Version: 1.0.29
  */
 
 using System.Collections.Immutable;
