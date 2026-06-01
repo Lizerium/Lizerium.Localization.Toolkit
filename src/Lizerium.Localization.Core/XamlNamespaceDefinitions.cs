9ac0ca9c2582e3aec67db18729c17a775393f112
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-01 14:47:37 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-01

--- src/Lizerium.Localization.Core/XamlNamespaceDefinitions.cs
+++ src/Lizerium.Localization.Core/XamlNamespaceDefinitions.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 31 мая 2026 14:55:30
- * Version: 1.0.45
+ * Last Updated: 01 июня 2026 14:47:28
+ * Version: 1.0.46
  */
 
 #if WINDOWS
