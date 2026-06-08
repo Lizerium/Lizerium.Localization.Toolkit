38d42ee55ddc54131e00e185c3fe5859f47b14a6
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-08 06:52:45 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-08

--- src/Lizerium.Localization.Core/PlaceholderAnalyzer.cs
+++ src/Lizerium.Localization.Core/PlaceholderAnalyzer.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 07 июня 2026 18:21:57
- * Version: 1.0.52
+ * Last Updated: 08 июня 2026 06:52:36
+ * Version: 1.0.53
  */
 
 using System.Text.RegularExpressions;
