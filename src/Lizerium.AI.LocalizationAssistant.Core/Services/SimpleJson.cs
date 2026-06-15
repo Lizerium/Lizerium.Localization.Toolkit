8d1020311bd44d08e1f209566c2ba82dcb428855
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-15 06:52:55 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-15

--- src/Lizerium.AI.LocalizationAssistant.Core/Services/SimpleJson.cs
+++ src/Lizerium.AI.LocalizationAssistant.Core/Services/SimpleJson.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 14 июня 2026 16:35:39
- * Version: 1.0.59
+ * Last Updated: 15 июня 2026 06:52:46
+ * Version: 1.0.60
  */
 
 using System.Globalization;
