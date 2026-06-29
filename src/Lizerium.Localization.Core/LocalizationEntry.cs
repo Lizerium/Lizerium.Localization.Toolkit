29de1e66107864feadb8f86f23bacbdc875088a2
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-29 06:53:27 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-29

--- src/Lizerium.Localization.Core/LocalizationEntry.cs
+++ src/Lizerium.Localization.Core/LocalizationEntry.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 28 июня 2026 11:43:17
- * Version: 1.0.73
+ * Last Updated: 29 июня 2026 06:53:17
+ * Version: 1.0.74
  */
 
 namespace Lizerium.Localization.Core;
