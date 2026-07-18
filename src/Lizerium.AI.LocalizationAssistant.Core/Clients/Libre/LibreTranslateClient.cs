fa6010fb7bc45758f61f744c529e383c3cd5dc26
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-18 07:27:44 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-18

--- src/Lizerium.AI.LocalizationAssistant.Core/Clients/Libre/LibreTranslateClient.cs
+++ src/Lizerium.AI.LocalizationAssistant.Core/Clients/Libre/LibreTranslateClient.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 17 июля 2026 11:00:37
- * Version: 1.0.92
+ * Last Updated: 18 июля 2026 07:27:34
+ * Version: 1.0.93
  */
 
 using Lizerium.AI.LocalizationAssistant.Core.Components.Ollama;
