51f6efe5c0557af77c6aafd67b4eff0661fc9af7
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-02 06:53:00 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-02

--- src/Lizerium.Localization.Generator/CodeBuilder.cs
+++ src/Lizerium.Localization.Generator/CodeBuilder.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 01 августа 2026 06:53:07
- * Version: 1.0.107
+ * Last Updated: 02 августа 2026 06:52:50
+ * Version: 1.0.108
  */
 
 using System.Collections.Generic;
