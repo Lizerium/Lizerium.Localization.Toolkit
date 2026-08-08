7496d8e7e44d8f06f199a72957917ca791d94671
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-08 06:53:00 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-08

--- src/Lizerium.AI.LocalizationAssistant.Core/Clients/Ollama/OllamaClient.cs
+++ src/Lizerium.AI.LocalizationAssistant.Core/Clients/Ollama/OllamaClient.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 07 августа 2026 08:23:59
- * Version: 1.0.113
+ * Last Updated: 08 августа 2026 06:52:50
+ * Version: 1.0.114
  */
 
 using System.Text;
