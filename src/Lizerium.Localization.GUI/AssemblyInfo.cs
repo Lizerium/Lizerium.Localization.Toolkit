57f69925071e910bf63e49535e533b2d3c397a14
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-14 08:17:04 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-14

--- src/Lizerium.Localization.GUI/AssemblyInfo.cs
+++ src/Lizerium.Localization.GUI/AssemblyInfo.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 13 августа 2026 06:52:40
- * Version: 1.0.119
+ * Last Updated: 14 августа 2026 08:16:54
+ * Version: 1.0.120
  */
 
 using System.Windows;
