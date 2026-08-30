424b55c5a1446476dbdba03dba3b7ac3c0484fe3
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-30 06:53:02 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-30

--- src/Lizerium.Localization.Core/ResxReader.cs
+++ src/Lizerium.Localization.Core/ResxReader.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 29 августа 2026 06:52:42
- * Version: 1.0.135
+ * Last Updated: 30 августа 2026 06:52:52
+ * Version: 1.0.136
  */
 
 using System.IO;
