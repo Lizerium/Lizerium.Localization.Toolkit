fd63b867685666bb9643cf4d71a39ee907743dc4
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-09-10 10:07:40 +0300

    Auto-update headers via pre_commit.ps1 - 2026-09-10

--- src/Lizerium.Localization.Generator/IsExternalInit.cs
+++ src/Lizerium.Localization.Generator/IsExternalInit.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 09 сентября 2026 06:52:41
- * Version: 1.0.146
+ * Last Updated: 10 сентября 2026 10:07:30
+ * Version: 1.0.147
  */
 
 namespace System.Runtime.CompilerServices;
